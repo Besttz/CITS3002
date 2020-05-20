e323ad13ba8a4dab186765f4a2ff830cc86a94db
Besttz <12153492+Besttz@users.noreply.github.com>
2020-05-20 14:50:25 +0000

    1.3 Code Transed, debugging

--- Py/s.py
+++ Py/s.py
@@ -14,15 +14,16 @@ de = ','
 
 
 class Route:
+
     def __init__(self, a1, a2, a3, a4, a5, a6, a7, a8):
-        fromS = a1
-        departH = a2
-        departM = a3
-        name = a4
-        platform = a5
-        arriveH = a6
-        arriveM = a7
-        destination = a8
+        self.fromS = a1
+        self.departH = a2
+        self.departM = a3
+        self.name = a4
+        self.platform = a5
+        self.arriveH = a6
+        self.arriveM = a7
+        self.destination = a8
 
 
 def readTT():
@@ -87,15 +88,15 @@ def findNextRoute(h, m, routeNo, toRecordNextStation):
             continue
         if toRecordNextStation and not checkR.destination == routeNext[routeNo]:
             continue
-        if toRecordNextStation and checkR.destination == routeNext[routeNo]:
+        if not toRecordNextStation and checkR.destination == routeNext[routeNo]:
             continue
         return i
     return -1
 
 
 def genMsg(r, dest):
-    msg = "0,"+r.arriveH+de+r.arriveM+de+r.name+de+r.destination+de+dest + \
-        de+r.fromS+de+r.departH+de+r.departM+de+r.platform+de+udpPort+de+sName
+    msg = "0,"+(str)(r.arriveH)+de+(str)(r.arriveM)+de+(str)(r.name)+de+(str)(r.name)+de+dest + \
+        de+(str)(r.fromS)+de+(str)(r.departH)+de+(str)(r.departM)+de+(str)(r.platform)+de+(str)(udpPort)+de+sName
     return msg
 
 
@@ -104,8 +105,8 @@ def genTransMsg(oldMsg, r):
     msg = '' + totalRoute + de
     for i in range(1, len(oldMsg) - 2):
         msg += oldMsg[i] + de
-    msg += r.arriveH+de+r.arriveM+de+r.name+de+r.destination+de+oldMsg[5] \
-        +de+ sName +de+r.departH+de+r.departM+de+r.platform+de+udpPort+de+sName
+    msg += (str)(r.arriveH)+de+(str)(r.arriveM)+de+(str)(r.name)+de+(str)(r.name)+de+oldMsg[5] \
+        + de + sName + de+(str)(r.departH)+de+(str)(r.departM)+de+r.platform+de+(str)(udpPort)+de+sName
     return msg
 
 
@@ -114,8 +115,10 @@ def genForwardMsg(oldMsg, r):
     msg = ''
     for i in range(totalRoute * 9 + 1):
         msg += oldMsg[i] + de
-    msg += r.arriveH+de+r.arriveM+de+r.name+de+r.destination+de+oldMsg[5] \
-        +de+ oldMsg[6 + totalRoute * 9] +de+oldMsg[7 + totalRoute * 9]+de+oldMsg[8 + totalRoute * 9]+de+oldMsg[9 + totalRoute * 9]+de+udpPort+de+sName
+    msg += (str)(r.arriveH)+de+(str)(r.arriveM)+de+(str)(r.name)+de+(str)(r.destination)+de+oldMsg[5] \
+        + de + oldMsg[6 + totalRoute * 9] + de+oldMsg[7 + totalRoute * 9]+de + \
+        oldMsg[8 + totalRoute * 9]+de + \
+        oldMsg[9 + totalRoute * 9]+de+(str)(udpPort)+de+sName
     return msg
 
 
@@ -137,7 +140,68 @@ def runTCP():
             print("TCP Station Name: " + request)
             if len(request) > 0:
                 readTT()
-            response = "HTTP/1.1 404 Not Found \n" + "Content-Type: text/html\n" + "Content-Length: 29" + "\n\n" + "<h1>Request Not Correct!</h1>"
+                msg = []
+                for i in range(len(routeName)):
+                    tRouteNo = findNextRoute(0, 0, i, True)
+                    tRouteNo2 = -1
+                    if not routeTerminal[i]:
+                        tRouteNo2 = findNextRoute(0, 0, i, False)
+                    if tRouteNo != -1:
+                        msg.append(genMsg(timeTable[i][tRouteNo], request))
+                    if tRouteNo2 != -1:
+                        msg.append(genMsg(timeTable[i][tRouteNo2], request))
+                answer = ''
+                if len(msg) == 0:
+                    answer = "No more available trip today!"
+                else:
+                    udpTsocket = socket.socket(
+                        socket.AF_INET, socket.SOCK_DGRAM)
+                    udpTsocket.bind(("0.0.0.0", 9000))
+                    for adj in adjPort:
+                        for ms in msg:
+                            udpTsocket.sendto(str.encode(ms), ('localhost', (int)(adj)))
+                            print("T-UDP: Send "+ms+" to "+adj)
+                    timeOut = False
+                    finalR = []
+                    udpTsocket.settimeout(10)
+                    while not timeOut:
+                        try:
+                            data, addr = udpTsocket.recvfrom(1024)
+                            finalR.append(data)
+                        except socket.timeout:
+                            timeOut = True
+                    if len(finalR) > 0:
+                        # print("T-UDP: Received data from destnation")
+                        answer += "<h1>Total Route: " + len(finalR) + "</h1>\n"
+                        tTime = []
+                        for i in range(len(finalR)):
+                            data = finalR[i].split(de)
+                            transTime = (int)(data[0])
+                            answer += "<p><br>Route: " + \
+                                (i + 1) + ", Transfer " + \
+                                data[0] + " time(s): <br>"
+                            for j in range(transTime+1):
+                                answer += data[7 + j * 9] + ":" + data[8 + j * 9] + " at " + data[6 + j * 9] + " , " + data[9 + j * 9] + " board " + \
+                                    data[3 + j * 9] + ", arrive " + data[4 + j * 9] + " at " + \
+                                    data[1 + j * 9] + ":" + \
+                                    data[2 + j * 9] + ".</p>\n"
+                            usedH = (int)(
+                                data[1 + transTime * 9])-(int)(data[7])
+                            usedM = (int)(
+                                data[2 + transTime * 9])-(int)(data[8])
+                            tTime.append(60*usedH+usedM)
+                        minTime = min(tTime)
+                        minTimeNo = tTime.index(minTime)
+                        answer += "<h2>The Fastest Route: " + \
+                            (minTimeNo + 1) + "</h2>\n"
+                    else:
+                        answer = "<h1>Can't Find a Route</h1>"
+                    udpTsocket.close
+                response = "HTTP/1.1 200 ok \n" + "Content-Type: text/html\n" + "Content-Length: " \
+                    + (str)(len(answer)) + "\n\n" + answer
+            else:
+                response = "HTTP/1.1 404 Not Found \n" + "Content-Type: text/html\n" + \
+                    "Content-Length: 29" + "\n\n" + "<h1>Request Not Correct!</h1>"
             clientsocket.send(response.encode('utf-8'))
             clientsocket.close()
         except socket.timeout:
@@ -145,13 +209,78 @@ def runTCP():
             # continue
             continue
 
-    # msg = '欢迎访问菜鸟教程！' + "\r\n"
-    # clientsocket.close()
+    clientsocket.close()
 
 
 def runUDP():
-    for i in range(5):
-        print(i)
+    udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udpsocket.bind(("0.0.0.0", (int)(udpPort)))
+    print("Start to listen UDP at " + udpPort)
+    while True:
+        # print('wating for message...')
+        recvdata, addr = udpsocket.recvfrom(1024)
+        readTT()
+        print("UDP: Received: " + (str)(recvdata))
+        data = (str)(recvdata).split(de)
+        totalTrans = (int)(data[0])
+        routeN = data[3 + 9 * totalTrans]
+        targetStation = data[4 + 9 * totalTrans]
+        finalDest = data[5 + 9 * totalTrans]
+        arriveH = (int)(data[1 + 9 * totalTrans])
+        arriveM = (int)(data[2 + 9 * totalTrans])
+        if not targetStation == sName or totalTrans > 4:
+            continue
+        if finalDest == sName:
+            msg = ""
+            for i in range(len(data)-2):
+                msg += data[i]+de
+            msg += udpPort+de+sName
+            print("UDP: Reply : " + msg)
+            udpsocket.sendto(str.encode(msg), ('localhost', 9000))
+        else:
+            print("UDP: Ready to forward")
+            routeNo = -1
+            for routename in routeName:
+                if routename == routeN:
+                    routeNo = routeName.index(routename)
+                    break
+            forwardable = True
+            if routeNo == -1 or (routeTerminal[routeNo] and routeNext[routeNo] == data[-1]):
+                forwardable = False
+            msgs = []
+            if forwardable:
+                toTheRecordStation = True
+                if routeNext[routeNo] == data[-1]:
+                    toTheRecordStation = False
+                nextR = findNextRoute(
+                    arriveH, arriveM, routeNo, toTheRecordStation)
+                if nextR != -1:
+                    cR = timeTable[routeNo][nextR]
+                    msgs.append(genForwardMsg(data, cR))
+            if len(routeName) > 1:
+                print("UDP: Ready to gen transfer")
+                for i in range(len(routeName)):
+                    if i == routeNo:
+                        continue
+                    tRouteNo = findNextRoute(arriveH, arriveM, i, True)
+                    tRouteNo2 = -1
+                    if not routeTerminal[i]:
+                        tRouteNo2 = findNextRoute(arriveH, arriveM, i, False)
+                    if tRouteNo != -1:
+                        cR = timeTable[i][tRouteNo]
+                        msg.append(genTransMsg(data, cR))
+                    if tRouteNo2 != -1:
+                        cR = timeTable[i][tRouteNo2]
+                        msg.append(genTransMsg(data, cR))
+            if len(msgs) > 0:
+                for adj in adjPort:
+                    for ms in msgs:
+                        udpsocket.sendto(str.encode(ms), ('localhost', (int)(adj)))
+                        print("UDP: Send "+ms+" to "+adj)
+
+        # udpsocket.sendto('[%s] %s'%(ctime(),data),addr)
+        # print('...received from and retuned to:', addr)
+    udpsocket.close()
 
 
 readTT()
@@ -161,6 +290,3 @@ tcp.start()
 udp.start()
 tcp.join()
 udp.join()
-# print(timeTable)
-# re = input("Enter URI")  # TEST
-# print(parseRequest(re))  # TEST
